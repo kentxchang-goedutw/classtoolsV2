06af9ecda44ac3bf23221b851bf7eef03ee28e9d
kentxchang-goedutw <kentxchang@go.edu.tw>
2025-10-04 15:41:16 +0800

    更新 firebase-config.js via EZPage

---
+++ firebase-config.js
@@ -0,0 +1,23 @@
+/**
+ * Firebase 設定檔
+ *
+ * 使用說明：
+ * 1. 將此檔案中的設定值改為您自己的 Firebase 專案設定
+ * 2. 在 HTML 檔案中引入此設定檔（在 Firebase SDK 之後，主程式之前）
+ *
+ * 如何取得這些設定值：
+ * 1. 前往 Firebase Console: https://console.firebase.google.com/
+ * 2. 選擇您的專案
+ * 3. 點擊專案設定（齒輪圖示）
+ * 4. 在「您的應用程式」區域中找到「SDK 設定和設定」
+ * 5. 複製設定值並貼到下方
+ */
+
+const __firebase_config = JSON.stringify({
+  apiKey: "AIzaSyC_Y1WgUfK2nAqEFda2fySEZYp5flYd54A",
+  authDomain: "test-f7558.firebaseapp.com",
+  projectId: "test-f7558",
+  storageBucket: "test-f7558.firebasestorage.app",
+  messagingSenderId: "672101127125",
+  appId: "1:672101127125:web:65c409c3098b7c5a4b235c"
+});
